73847b0691592f25217e7fcc2d4fbc85765c957f
JRUCSD <97696224+JRUCSD@users.noreply.github.com>
2022-02-02 20:38:38 -0800

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,11 @@
+//I have the code written up already but before I move over to VSCode here is the order for the remaining steps
+//Copy the code to the cloned java files
+//Run the tester locally
+//Push changes
+//scp the repo folder to ieng6
+//Recompile & run the tester remotely
+//Edit the files to pass the JUnit tests
+//Push changes
+//scp the repo folder to ieng6 again
+//Recompile & rerun the tester remotely
+//Log off ieng6 & submit the skill demo
